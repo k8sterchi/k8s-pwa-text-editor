f7ff60b728136f3984a24e77f8d13da35339a814
k8 <katesterchi@gmail.com>
2023-11-01 13:00:28 -0500

    changed output path so that it did not mess up file structure

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = () => {
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, './client/dist'),
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
